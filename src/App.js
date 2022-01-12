4658a47b48e726f094d94700ba4f98fe12d15cec
Caleb Pirkle <caleb@rftb.agency>
2022-01-12 11:44:46 -0600

    frontend progress

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Rank from './components/rank/Rank';
 import FaceRecognition from './components/faceRecognition/FaceRecognition';
 import Particles from "react-tsparticles";
 import { particlesConfig } from './particlesConfig/particlesConfig';
-import Clarifai, { GENERAL_MODEL } from 'clarifai';
+import Clarifai from 'clarifai';
 import './App.css';
 
 const app = new Clarifai.App({
@@ -18,33 +18,61 @@ class App extends Component {
     super(props);
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
     }
   }
+
   // handle user input
   onInputChange = (event) => {
     console.log('input changed')
     this.setState({ input: event.target.value });
   }
+
+  // get the face location bounding box
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data?.outputs[0].data.regions[0].region_info.bounding_box
+    console.log(clarifaiFace);
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+    // data = data?.outputs[0].data.regions;
+    // console.log(data)
+    // let boxes = data.map(element => {
+    // return {
+    //   leftCol: element.region_info.bounding_box.left_col * width,
+    //   topRow: element.region_info.bounding_box.top_row * height,
+    //   rightCol: width - (element.region_info.bounding_box.right_col * width),
+    //   bottomRow: height - (element.region_info.bounding_box.bottom_row * height)
+    // };
+    // });
+    // console.log(boxes);
+  }
+
+  // display the face bounding box
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  }
+
   // handle button click
   onButtonClick = () => {
     console.log('click');
-
-    this.setState({imageUrl: this.state.input});
-
+    this.setState({ imageUrl: this.state.input });
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      // handle response
       .then(
-        function (response) {
-          // handle resp
+        response => {
           console.log(response)
-          response = response?.outputs[0].data.regions[0].region_info?.bounding_box
-          console.log(response)
-        },
-        function (err) {
-          // handle err
-          console.log('something went wrong', err)
-        }
-      )
+          this.displayFaceBox(this.calculateFaceLocation(response))
+        })
+      // handle error
+      .catch(err => console.log('something went wrong', err))
   }
 
   render() {
@@ -54,7 +82,7 @@ class App extends Component {
         <Navigation />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonClick={this.onButtonClick} />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
--- src/components/faceRecognition/FaceRecognition.js
+++ src/components/faceRecognition/FaceRecognition.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
     return (
         <div className='center-1 ma'>
-            <div className='center absolute mt2'>
-                <img src={imageUrl} alt='user submitted' width="500px" height="auto" />
+            <div className='center absolute mt2' style={{width:'300px'}}>
+                <img id='inputimage' src={imageUrl} alt='user submitted' width="auto" height="auto" />
+                <div className='bounding-box' style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
             </div>
         </div>
     )
